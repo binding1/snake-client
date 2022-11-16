240a1c987b3243302220fb355cef44dddaf8bb5d
binding1 <jaey.pro@gmail.com>
2022-11-16 22:06:54 +0000

    updated event listener so user inputs are now used in the game to move the snake

--- input.js
+++ input.js
@@ -1,19 +1,37 @@
+const { connect } = require("http2");
+const { stdin } = require("process");
+
+let connection;
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
+    console.log('Game exiting...')
     process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'd') {
+    connection.write('Move: right');
   }
-};
+}; 
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
+  stdin.on("data", key => {
+    handleUserInput(key);
+  });
   return stdin;
 };
 
 module.exports = {
   setupInput
 };
+
